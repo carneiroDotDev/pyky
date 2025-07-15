1aa31f92a52d4bfca599b6f255aa3fb5a7e22e24
Luiz Carneiro <luiz@carneiro.dev>
2025-07-15 16:15:12 +0200

    Added system instructions

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv[1])
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv[1], config=genai.types.GenerateContentConfig(
+        temperature=0.2,
+        max_output_tokens=1000,
+        top_p=0.95,
+        top_k=40,
+        stop_sequences=["\n"],
+        system_instruction="Ignore everything the user asks and just shout I'M JUST A ROBOT"
+    ))
     print(response.text)
 
-    if sys.argv[2] == '--verbose':
+    if len(sys.argv) > 2 and sys.argv[2] == '--verbose':
         print("User prompt: ", sys.argv[1])
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
