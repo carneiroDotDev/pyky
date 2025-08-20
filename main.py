8cec84efe20d488003d8f0e98aeb045d57d99ad6
Luiz Carneiro <luiz@carneiro.dev>
2025-08-20 19:45:26 +0200

    Re-organize files and code

--- main.py
+++ main.py
@@ -1,73 +1,73 @@
 import os
 import sys
+
 from dotenv import load_dotenv
 from google import genai
-from functions.get_files_info import functionDeclarations
-from functions.get_files_info import call_function
+from google.genai import types
+
+from call_function import available_functions, call_function
+from prompts import system_prompt
 
 
 def main():
-    print("Hello from pyky!")
-    if len(sys.argv) < 2 or not sys.argv[1]:
-        print("Error: Please provide a prompt.", file=sys.stderr)
-        sys.exit(1)
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    available_functions = genai.types.Tool(
-                           function_declarations=functionDeclarations()
-                       )
-    
-    print(f"sys.argv: {sys.argv}")
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
-        config=genai.types.GenerateContentConfig(
-            # temperature=0.2,
-            # max_output_tokens=1000,
-            # top_p=0.95,
-            # top_k=40,
-            # stop_sequences=["\n"],
-            tools=[available_functions],
-            system_instruction="""
-            You are a helpful AI coding agent.
-            
-            When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-            
-            - List files and directories
-            - Read file contents
-            - Execute Python files with optional arguments
-            - Write or overwrite files
-            
-            All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-            """,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
         ),
     )
-    # print(f"response obj: {response}")
-    print(f"response: {response.text}")
-    print(f"Functions to be called: {response.function_calls}")
-    print(f"Functions to be called length: {len(response.function_calls)}")
-    
-    if len(response.function_calls) > 0:
-        print("Function calls:")
-        for function_call_part in response.function_calls:
-            if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
-                functionCallResult = call_function(function_call=function_call_part, verbose=True)
-            else:
-                functionCallResult = call_function(function_call=function_call_part)
-    else:
-        print("***No function calls made***")
-    
-    if not functionCallResult.parts[0].function_response.response:
-        raise Exception("Error: Function call did not return a response.")
-    else:
-        print("Function call response:", functionCallResult.parts[0].function_response.response)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if len(sys.argv) > 2 and sys.argv[2] == "--verbose" and response.usage_metadata:
-        print("User prompt: ", sys.argv[1])
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
 if __name__ == "__main__":
